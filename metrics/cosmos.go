53bb6fba1147807b54542a4d185e57dd92e618c9
David Nix <hello@davidnix.io>
2023-05-09 12:06:58 -0600

    Add validator status metric

--- metrics/cosmos.go
+++ metrics/cosmos.go
@@ -8,30 +8,61 @@ import (
 
 // Cosmos records metrics for Cosmos chains
 type Cosmos struct {
-	rpcHeightGauge *prometheus.GaugeVec
+	heightGauge    *prometheus.GaugeVec
+	valStatusGauge *prometheus.GaugeVec
 }
 
 func NewCosmos() *Cosmos {
 	return &Cosmos{
-		rpcHeightGauge: prometheus.NewGaugeVec(
+		heightGauge: prometheus.NewGaugeVec(
 			prometheus.GaugeOpts{
 				Name: prometheus.BuildFQName(Namespace, CosmosSubsystem, "latest_block_height"),
 				Help: "Latest block height of a cosmos node.",
 			},
-			[]string{"chain_id", "source"}, // rpc height labels
+			// labels
+			[]string{"chain_id", "source"},
+		),
+		valStatusGauge: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: prometheus.BuildFQName(Namespace, CosmosSubsystem, "val_jailed_status"),
+				Help: "Whether a validator is active, jailed, or tombstoned.",
+			},
+			// labels
+			[]string{"chain_id", "source", "address", "status"},
 		),
 	}
 }
 
 // SetNodeHeight records the block height on the public_rpc_node_height gauge.
-func (c *Cosmos) SetNodeHeight(chain string, rpcURL url.URL, height float64) {
-	source := rpcURL.Hostname() + rpcURL.Path
-	c.rpcHeightGauge.WithLabelValues(chain, source).Set(height)
+func (c *Cosmos) SetNodeHeight(chain string, restURL url.URL, height float64) {
+	source := restURL.Hostname() + restURL.Path
+	c.heightGauge.WithLabelValues(chain, source).Set(height)
+}
+
+// ValStatus is the status of a validator.
+type ValStatus string
+
+const (
+	ValStatusActive     ValStatus = "active"
+	ValStatusJailed     ValStatus = "jailed"
+	ValStatusTombstoned ValStatus = "tombstoned"
+)
+
+// SetValidatorStatus records the status of a validator. Gauge set to 1 if the validator is jailed or tombstoned.
+// In this context, "active" does not mean part of the validator active set.
+func (c *Cosmos) SetValidatorStatus(chain, consaddress string, restURL url.URL, status ValStatus) {
+	source := restURL.Hostname() + restURL.Path
+	var value float64
+	if status != ValStatusActive {
+		value = 1
+	}
+	c.valStatusGauge.WithLabelValues(chain, source, consaddress, string(status)).Set(value)
 }
 
 // Metrics returns all metrics for Cosmos chains to be added to a Prometheus registry.
 func (c *Cosmos) Metrics() []prometheus.Collector {
 	return []prometheus.Collector{
-		c.rpcHeightGauge,
+		c.heightGauge,
+		c.valStatusGauge,
 	}
 }
