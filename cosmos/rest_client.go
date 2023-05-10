32f872dd4067dbd5b93b1f8b27d3421d97c706ce
David Nix <hello@davidnix.io>
2023-05-10 16:15:39 -0600

    Rename client to rest client

--- cosmos/rest_client.go
+++ cosmos/rest_client.go
@@ -6,18 +6,23 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type RestClient struct {
-	httpDo func(req *http.Request) (*http.Response, error)
+	baseURL url.URL
+	httpDo  func(req *http.Request) (*http.Response, error)
 }
 
-func NewRestClient(c *http.Client) *RestClient {
-	return &RestClient{httpDo: c.Do}
+func NewRestClient(c *http.Client, baseURL url.URL) *RestClient {
+	return &RestClient{
+		baseURL: baseURL,
+		httpDo:  c.Do,
+	}
 }
 
 // response must be a pointer to a datatype (typically a struct)
-func (c *RestClient) get(ctx context.Context, url string, response any) error {
+func (c RestClient) get(ctx context.Context, url string, response any) error {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return fmt.Errorf("malformed request: %w", err)
